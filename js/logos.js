e64cc12f7d4117112bde4bb5d756d06200e09de5
Your Namepelle <skogsbergdesign@gmail.com>
2021-05-23 19:20:28 +0200

    s

--- js/logos.js
+++ js/logos.js
@@ -1,64 +1,27 @@
 
 		$(function(){
 		  $(".text-logo-svg").load("images/icons/text-logo.svg"); 
+		  $(".next-play-circle").load("images/icons/next_play_circle.svg"); 			
+		  //$(".zoom-icon").load("images/icons/zoom-icon.svg"); 		
+		  $(".akg-logo").load("akg/images/akg-logo.svg"); 			
+		  $(".converse-logo").load("Converse/images/converse-logo.svg"); 						
+		  $(".bagheera-logo").load("Bagheera/images/bagheera-logo.svg"); 						
+		  $(".poc-logo").load("poc/images/poc-logo.svg"); 				
+		  $(".specialized-logo").load("Specialized/images/specialized-logo.svg"); 				
+		  $(".haglofs-logo").load("haglofs/images/haglofs-logo.svg"); 				
+		  $(".alcon-logo").load("alcon/images/alcon-logo.svg"); 						
+		  $(".p-logo").load("images/icons/p-logo.svg");		
+		  $(".cog-logo").load("images/icons/cog.svg"); 					
+		  $(".arrow-rotate-right").load("images/icons/rotate_right_black.svg"); 				
+		  $(".arrow-rotate-left").load("images/icons/rotate_left_black.svg"); 				
+		  $(".color-circle").load("images/icons/color_circle.svg"); 				
+		  $(".converse-process-05").load("Converse/images/process/modeling01.svg"); 							
+		  $(".bulb-icon").load("images/process/bulb.svg"); 					
+		  $(".scroll-down").load("images/icons/scrolldown.svg"); 					
+		  $(".akg-functions1").load("akg/images/functions.svg"); 					
+		  $(".akg-functions2").load("akg/images/functions2.svg"); 					
+		  $(".next-circle").load("images/icons/next_circle.svg"); 		  
 		});
-		$(function(){
-		  $(".next-play-circle").load("images/icons/next_play_circle.svg"); 
-		});
-		$(function(){
-		  $(".zoom-icon").load("images/icons/zoom-icon.svg"); 
-		});
-		$(function(){
-		  $(".akg-logo").load("akg/images/akg-logo.svg"); 
-		});
-		$(function(){
-		  $(".converse-logo").load("Converse/images/converse-logo.svg"); 
-		});			
-		$(function(){
-		  $(".bagheera-logo").load("Bagheera/images/bagheera-logo.svg"); 
-		});		
-		$(function(){
-		  $(".poc-logo").load("poc/images/poc-logo.svg"); 
-		});
-		$(function(){
-		  $(".specialized-logo").load("Specialized/images/specialized-logo.svg"); 
-		});
-		$(function(){
-		  $(".haglofs-logo").load("haglofs/images/haglofs-logo.svg"); 
-		});
-		$(function(){
-		  $(".alcon-logo").load("alcon/images/alcon-logo.svg"); 
-		});		
-		$(function(){
-		  $(".p-logo").load("images/icons/p-logo.svg"); 
-		});
-		$(function(){
-		  $(".cog-logo").load("images/icons/cog.svg"); 
-		});	
-		$(function(){
-		  $(".arrow-rotate-right").load("images/icons/rotate_right_black.svg"); 
-		});
-		$(function(){
-		  $(".arrow-rotate-left").load("images/icons/rotate_left_black.svg"); 
-		});
-		$(function(){
-		  $(".color-circle").load("images/icons/color_circle.svg"); 
-		});
-		$(function(){
-		  $(".converse-process-05").load("Converse/images/process/modeling01.svg"); 
-		});			
-		$(function(){
-		  $(".bulb-icon").load("images/process/bulb.svg"); 
-		});	
-		$(function(){
-		  $(".scroll-down").load("images/icons/scrolldown.svg"); 
-		});	
-		$(function(){
-		  $(".akg-functions1").load("akg/images/functions.svg"); 
-		});	
-		$(function(){
-		  $(".akg-functions2").load("akg/images/functions2.svg"); 
-		});	
-		$(function(){
-		  $(".next-circle").load("images/icons/next_circle.svg"); 
-		});		
+		
+
+				
