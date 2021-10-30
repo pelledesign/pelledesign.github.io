28557439cae37797a998484eb3b43989e34fb7f7
Your Namepelle <skogsbergdesign@gmail.com>
2021-10-30 16:32:44 +0200

    d

--- js/jquery-scroll-snap.js
+++ js/jquery-scroll-snap.js
@@ -2,7 +2,7 @@
  * https://github.com/jpcurrier/jquery-scroll-snap !*/
  
  $('#header, #header-mobile').addClass('auto-hide-header'); 
- console.log('5');
+
 ( function( $ ){
   $.fn.scrollSnap = function( options ){
 
